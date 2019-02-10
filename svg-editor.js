3aa2eb0109a82b41fb98bedfedd677c4d2ddbf38
James Scott-Brown <james@jamesscottbrown.com>
2019-02-10 12:55:31 +0000

    tidy code

--- svg-editor.js
+++ svg-editor.js
@@ -1,15 +1,15 @@
-var parameters = {'x': 60, "y": 60, "d": 5};
+let parameters = {'x': 60, "y": 60, "d": 5};
 
 function importSVG(svgText) {
     // Load an SVg or parametric SVG from a string
 
     // clear param list
-    var codePanel = d3.select("#parameter-list");
+    const codePanel = d3.select("#parameter-list");
     codePanel.node().innerHTML = "";
     codePanel.append("ul").attr("id", "paramList");
 
     // clear element list
-    var elements = d3.select("#elements");
+    const elements = d3.select("#elements");
     elements.node().innerHTML = "";
     elements.append("ul").attr("id", "elementList");
 
@@ -19,7 +19,7 @@ function importSVG(svgText) {
         return;
     }
 
-    var svg = document.querySelector('svg');
+    const svg = document.querySelector('svg');
 
     if (svgText.includes("<svg")){
         svg.outerHTML = svgText;
@@ -27,9 +27,9 @@ function importSVG(svgText) {
         svg.innerHTML = svgText;
     }
 
-        var children = svg.childNodes;
-    for (var i = 0; i < children.length; i++) {
-        var child = children[i];
+    let children = svg.childNodes;
+    for (let i = 0; i < children.length; i++) {
+        const child = children[i];
         // TODO: need to igo deeper for <g>
         // child.id
         // child.classList
@@ -39,7 +39,7 @@ function importSVG(svgText) {
     }
 
     // Load default parameter values
-    let defaultAttribute = svg.attributes["parametric:defaults"];
+    const defaultAttribute = svg.attributes["parametric:defaults"];
     if (defaultAttribute){
         let assignments = defaultAttribute.nodeValue.split(";");
         let param, value;
@@ -61,33 +61,32 @@ function importSVG(svgText) {
 
 function addNodeToList(child){
     // Add list tiem for a tag/element
-        element_list = d3.select("#elementList");
+        let element_list = d3.select("#elementList");
         getParameters(child.attributes);
 
         if (child.tagName) {
-            var tag_item = element_list.append("li").text(child.tagName);
+            const tag_item = element_list.append("li").text(child.tagName);
 
             tag_item.append("button")
                 .text("-")
                 .datum(child)
-                .on("click", function(d){
-
+                .on("click", function () {
                     child.innerHTML = "";
                     d3.select(child).remove();
 
-                    var parentNode = this.parentNode;
+                    const parentNode = this.parentNode;
                     parentNode.innerHTML = "";
                     d3.select(parentNode).remove();
                 });
 
-            var sublist = tag_item.append("ul");
+            const sublist = tag_item.append("ul");
 
 
             if (child.attributes) {
-                for (var j = 0; j < child.attributes.length; j++) {
-                    var attribute = child.attributes[j];
+                for (let j = 0; j < child.attributes.length; j++) {
+                    const attribute = child.attributes[j];
 
-                    var name = attribute.name;
+                    let name = attribute.name;
 
                     // Don't display parameter if it a "parametric:" version of parameter also exists
                     if (child.attributes.getNamedItem("parametric:" + name)) {
@@ -107,15 +106,15 @@ function addNodeToList(child){
             .text("Add attribute")
             .datum({child: child, sublist: sublist})
             .on("click", function(d){
-                var name = prompt('Name');
+                const name = prompt('Name');
                 addItem(sublist, name, "", d.child)
             })
         }
 }
 
 function addElement(){
-    var type = prompt("Tag name");
-    var child = d3.select("svg").append(type).node();
+    const type = prompt("Tag name");
+    const child = d3.select("svg").append(type).node();
     addNodeToList(child);
 }
 
@@ -123,15 +122,15 @@ function addElement(){
 function updatePath(input, child){
     d3.select(input.parentNode).datum(input.value);
 
-    var children = Array.prototype.slice.call(input.parentNode.parentNode.children); // convert to array so can use map
-    var segmentStrings = children.map( function(n){ return d3.select(n).data() });
-    var segmentString = segmentStrings.join(" ");
+    const children = Array.prototype.slice.call(input.parentNode.parentNode.children); // convert to array so can use map
+    const segmentStrings = children.map( function(n){ return d3.select(n).data() });
+    const segmentString = segmentStrings.join(" ");
 
-    var segments = splitPath(segmentString);
+    const segments = splitPath(segmentString);
 
-    var isParameteric = false;
-    for (var i=0; i<segments.length; i++){
-        var stripped = segments[i].substr(1).replace(/ /g, '').replace(/,/g, '').replace(/'/g, '').replace(/-/g, '');
+    let isParameteric = false;
+    for (let i=0; i<segments.length; i++){
+        let stripped = segments[i].substr(1).replace(/ /g, '').replace(/,/g, '').replace(/'/g, '').replace(/-/g, '');
 
         if (this.value !== (+this.value).toString()){
             isParameteric = true;
@@ -155,16 +154,16 @@ function updateStyle(input, child){
 
     d3.select(input.parentNode).datum(input.value);
 
-    var children = Array.prototype.slice.call(input.parentNode.parentNode.children); // convert to array so can use map
-    var segmentStrings = children.map( function(n){ return d3.select(n).data() });
-    var segmentString = segmentStrings.join("; ");
+    const children = Array.prototype.slice.call(input.parentNode.parentNode.children); // convert to array so can use map
+    const segmentStrings = children.map( function(n){ return d3.select(n).data() });
+    const segmentString = segmentStrings.join("; ");
 
-    var segments = segmentString.split(';');
+    const segments = segmentString.split(';');
 
-    var isParameteric = false;
+    let isParameteric = false;
 
-    var field, val;
-    for (var i=0; i<segments.length; i++){
+    let field, val;
+    for (let i=0; i<segments.length; i++){
 
         [field, val] = segments[i].split(":");
 
@@ -200,11 +199,11 @@ function addItem(sublist, name, value, child) {
             .text("Path:")
             .datum({element: child, attribute_name: name}); // ?
 
-        var segmentsList = sublist.append("ul");
+        const segmentsList = sublist.append("ul");
 
-        var segments = splitPath(value);
+        const segments = splitPath(value);
 
-        var segmentItems = segmentsList.selectAll("li")
+        const segmentItems = segmentsList.selectAll("li")
             .data(segments)
             .enter()
             .append("li");
@@ -217,11 +216,11 @@ function addItem(sublist, name, value, child) {
 
         segmentItems.append("button")
             .text("-")
-            .on("click", function(d){
+            .on("click", function (){
                 this.value = " : ";
                 updatePath(this, child);
 
-                var parentNode = this.parentNode;
+                const parentNode = this.parentNode;
                 parentNode.innerHTML = "";
                 d3.select(parentNode).remove();
             });
@@ -248,11 +247,11 @@ function addItem(sublist, name, value, child) {
             .text("Style:")
             .datum({element: child, attribute_name: name}); // ?
 
-        var segmentsList = sublist.append("ul");
+        const segmentsList = sublist.append("ul");
 
-        var segments = value.split(';');
+        const segments = value.split(';');
 
-        var segmentItems = segmentsList.selectAll("li")
+        const segmentItems = segmentsList.selectAll("li")
             .data(segments)
             .enter()
             .append("li");
@@ -265,11 +264,11 @@ function addItem(sublist, name, value, child) {
 
         segmentItems.append("button")
             .text("-")
-            .on("click", function(d){
+            .on("click", function (){
                 this.value = "";
                 updateStyle(this, child);
 
-                var parentNode = this.parentNode;
+                const parentNode = this.parentNode;
                 parentNode.innerHTML = "";
                 d3.select(parentNode).remove();
             });
@@ -292,7 +291,7 @@ function addItem(sublist, name, value, child) {
 
 
     } else {
-        var subitem = sublist.append("li").text(name + ": ");
+        const subitem = sublist.append("li").text(name + ": ");
 
         subitem.append("input")
             .property("value", value)
@@ -328,7 +327,7 @@ function addItem(sublist, name, value, child) {
 function updateParamsList() {
 
     // add inputs for any newly-created parameters
-    for (var p in parameters) {
+    for (let p in parameters) {
 
         if (!d3.select("#parameter-" + p).empty()) {
             continue;
@@ -359,7 +358,7 @@ function updateParamsList() {
 
 
 function processExpressionTerm(term) {
-    var expression = term
+    const expression = term
         .replace(/\{/g, '')
         .replace(/\(/g, '')
         .replace(/\)/g, '')
@@ -372,9 +371,9 @@ function processExpressionTerm(term) {
         .replace(/,/g, ' ')
         .replace(/'/g, ' ');
 
-    var terms = expression.split(" ");
+    const terms = expression.split(" ");
 
-    for (var term in terms) {
+    for (let term in terms) {
         if (!terms[term] || !isNaN(terms[term])) {
             continue;
         }
@@ -394,15 +393,14 @@ function getParameters(attributes) {
         return;
     }
 
-    for (var j = 0; j < attributes.length; j++) {
-        var attribute = attributes[j];
+    // process only the contents of {}
+    const re = /\{(.+?)\}/g;
+
+    for (let j = 0; j < attributes.length; j++) {
+        const attribute = attributes[j];
 
         if (attribute.name.startsWith("parametric:")) {
-
-            // process only the contents of {}
-            var re = /\{(.+?)\}/g;
             attribute.nodeValue.replace(re, function(match, g1, g2) { processExpressionTerm(g1) });
-
         }
     }
 
@@ -412,24 +410,24 @@ function getParameters(attributes) {
 
 function applyParameters() {
     // Substitutes the values in parameters into the SVG
-    var svg = d3.select("parametric-svg").select("svg").node();
+    const svg = d3.select("parametric-svg").select("svg").node();
     // parametricSvg(svg, parameters);
 
-    var tagNames = ["rect", "circle", "ellipse", "line", "polyline", "polygon",
+    const tagNames = ["rect", "circle", "ellipse", "line", "polyline", "polygon",
                     "text", "tspan", "tref", "textPath", "altGlyph", "altGlyphDef", "altGlyphItem", "glyphRef",
                     "marker", "path"];
 
-    var re = /\{(.+?)\}/g;
+    const re = /\{(.+?)\}/g;
 
-    for (var i in tagNames){
+    for (let i in tagNames){
 
-        var tags = svg.getElementsByTagName(tagNames[i]);
+        const tags = svg.getElementsByTagName(tagNames[i]);
 
-        for (var j in tags){
-            var tag = tags[j];
-            for (var k in tag.attributes){
-                var name = tag.attributes[k].name;
-                var value = tag.attributes[k].nodeValue;
+        for (let j in tags){
+            let tag = tags[j];
+            for (let k in tag.attributes){
+                let name = tag.attributes[k].name;
+                let value = tag.attributes[k].nodeValue;
 
                 if (name && name.startsWith("parametric:")){
 
@@ -476,7 +474,7 @@ function setup() {
         });
 
     importSVG('<circle parametric:cx="{x + d}" parametric:cy="{y}" r="40" stroke="black" stroke-width="3" fill="blue" />\n' +
-        '  <circle parametric:cx="{x}" parametric:cy="{y}" r="40" stroke="black" stroke-width="3" fill="green" />\n')
+        '  <circle parametric:cx="{x}" parametric:cy="{y}" r="40" stroke="black" stroke-width="3" fill="green" />\n');
 
     applyParameters();
 }
@@ -484,10 +482,10 @@ function setup() {
 function splitPath(pathString){
     // Splits a SVG path d attribute into individual commands and return as an array
 
-    var i = 0;
-    var segments = [];
+    let i = 0;
+    let segments = [];
 
-    for (var k=1; k<pathString.length; k++){
+    for (let k=1; k<pathString.length; k++){
         // Only split on command character if followed by a space (or it is a Z at end of string)
         if (pathString.substr(k).match(/^[mlhvcsqa] /i) || pathString.substr(k).match(/^z$|^z /i) ){
             segments.push(pathString.substring(i, k).trim());
