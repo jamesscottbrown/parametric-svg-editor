bb1de10516f3d4331adf0536a64324e4cf59389e
James Scott-Brown <james@jamesscottbrown.com>
2020-05-30 22:36:18 +0100

    Updates

---
+++ precache-manifest.8bf071a46b5ac11dc58b5d94ab3b443f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8212483798a5842d0dbf4cfa4adc74fa",
+    "url": "/parametric-svg-editor/index.html"
+  },
+  {
+    "revision": "8cc70ec1ccb5acb7adaf",
+    "url": "/parametric-svg-editor/static/css/2.cbb6c2b0.chunk.css"
+  },
+  {
+    "revision": "e540d0364e2ff29ba602",
+    "url": "/parametric-svg-editor/static/css/main.f8acc1d7.chunk.css"
+  },
+  {
+    "revision": "8cc70ec1ccb5acb7adaf",
+    "url": "/parametric-svg-editor/static/js/2.a8aad66b.chunk.js"
+  },
+  {
+    "revision": "70031bd0dc02a3364b81779f69e088f6",
+    "url": "/parametric-svg-editor/static/js/2.a8aad66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e540d0364e2ff29ba602",
+    "url": "/parametric-svg-editor/static/js/main.600d44c0.chunk.js"
+  },
+  {
+    "revision": "e30cb6969651e2895552",
+    "url": "/parametric-svg-editor/static/js/runtime-main.aec70bd7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec59311f485fd09a585c262f2561f06e.js"
+  "/parametric-svg-editor/precache-manifest.8bf071a46b5ac11dc58b5d94ab3b443f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/parametric-svg-editor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
